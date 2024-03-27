00cc2c2998aa41cc7abaac6dce85efc3fe4b3728
unknown <tyler.kd.knapp@gmail.com>
2024-03-27 10:40:55 -0500

    Added GET, POST, PUT, and DELETE route handlers for userController

---
+++ controllers/userController.js
@@ -0,0 +1,68 @@
+const { User } = require('../models');
+
+module.exports = {
+    // GET all users
+    async getUsers(req, res) {
+        try {
+            const users = await User.find();
+            res.json(users);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    // GET a single user by its _id and populated thought and friend data
+    async getSingleUser(req, res) {
+        try {
+            const user = await User.findOne({ _id: req.params.userId })
+            .select('-__v')
+            .populate('thoughts').populate('friends');
+        if (!user) {
+            return res.status(404).json({ message: 'No user with that ID' });
+        }
+
+        res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    // POST a new user
+    async createUser(req, res) {
+        try {
+            const dbUserData = await User.create(req.body);
+            res.json(dbUserData);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    // PUT to update a user by its _id
+    async updateUser(req, res) {
+        try {
+            const user = await User.findOneAndUpdate(
+                { _id: req.params.userId },
+                { $set: req.body },
+                { runValidators: true, new: true }
+            );
+            if (!user) {
+                return res.status(404).json({ message: 'No user with this id!' });
+            }
+            res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    // DELETE to remove a user by its _id
+    async deleteUser(req, res) {
+        try {
+            const user = await User.findOneAndRemove({ _id: req.params.userId });
+
+            if (!user) {
+                return res.status(404).json({ message: 'No such user exists' })
+            }
+
+            res.json({ message: 'User successfully deleted' });
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
+    },
+}

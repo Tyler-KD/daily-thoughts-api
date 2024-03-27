4e90cf103527b0c15c5d3f4d93f03fa499ea96f1
unknown <tyler.kd.knapp@gmail.com>
2024-03-26 19:22:04 -0500

    Added files to config, controllers, models, routes, and utils files

---
+++ config/connection.js
@@ -0,0 +1,9 @@
+const { connect, connection } = require('mongoose');
+
+// Wrap Mongoose around local connection to MongoDB
+const connectionString = 'mongodb://127.0.0.1:27017/dailythoughts';
+
+connect(connectionString);
+
+// Export connection
+module.exports = connection;
